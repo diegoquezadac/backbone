7fee32273809e10b9daf9824e78fd37ba1b0b2a1
Diego <diego.quezadac@sansano.usm.cl>
2022-02-23 00:22:02 -0300

    implement basic CLI

--- src/app.py
+++ src/app.py
@@ -6,51 +6,34 @@
 ##### MAKER
 ##### <>
 
-import os
-from parser import get_variables, get_config
+import typer
+from shutil import rmtree
+from typing import Optional
+from backbone import build_backbone, get_variables
 
-def build_backbone():
-    # Get .conf data
+app = typer.Typer()
+
+@app.command()
+def build(root: Optional[str] = None):
+    if(root is None):
+        build_backbone()
+    else:
+        build_backbone(root)
+
+
+@app.command()
+def clean(root: Optional[str] = None):
     variables = get_variables()
-    config = get_config()
-    # Unpack variables
-    root = variables["root"]
-    courses = variables["courses"]
-    # Map from string to list
-    courses_list = courses.split(",")
-    # Define state variables
-    folders = list()
-    current_directory = ""
-    for course in courses_list:
-        # Create course folder
-        current_directory = f"{root}/{course}"
-        os.mkdir(current_directory)
-        # Build course backbone
-        for bone in config:
-            # Move down one directory
-            if(">" in bone):
-                # Mount last bone
-                current_directory = f"{current_directory}/{folders[-1]}"
-            # Move up one directory)
-            elif("<" in bone):
-                # Unmount last bone
-                current_directory = "/".join(current_directory.split("/")[:-1])
-            # Execute command
-            else:
-                command,argument = bone.split(":")
-                command,argument = command.strip(),argument.strip()
-                # Parse command
-                if(command == "f"):
-                    # Save folder
-                    folders.append(argument)
-                    # Create folder
-                    os.mkdir(f"{current_directory}/{argument}")
-                elif(command == "m" or command == "t"):
-                    # Create file
-                    new_file = open(f"{current_directory}/{argument}", "w")
-                    new_file.close()
-                else:
-                    raise Exception(f"{command} is an unknown command. Only f,m and t commands are available.")
+    folders = variables["courses"].split(",")
+    if(root is None):
+        root = variables["root"]
+    for folder in folders:
+        rmtree(f"{root}/{folder}")
 
-# LET'S BUILD THAT THING
-build_backbone()
+@app.command()
+def variables():
+    typer.echo(get_variables())
+
+
+if __name__ == "__main__":
+    app()
